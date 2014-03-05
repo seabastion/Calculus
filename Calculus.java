3cab2351f239ba846139ba61a36ccdce9c506846
Sebastian <v8sebas@gmail.com>
2014-03-04 22:40:06 -0600

    first commit

---
+++ Calculus.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+/*
+ * This class defines several calculus functions
+ * 
+ * @author Sebastian Hall
+ * @date March 4th, 2014
+ */
+ public class Calculus{
+	 //Constructor
+	 public Calculus{}
+	 //integral function lim 1 is limit on the bottom of the integral dx
+	 //determines whether the integray is being taken in terms of x or y
+	 public double integral(double lim1, double lim2, boolean dx, String f){
+		 
+	 }
+	 
+}
