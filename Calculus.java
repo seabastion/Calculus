bd4a3babb79fb4c7517553f6415648547d5d63f3
seabastion <v8sebas@gmail.com>
2014-03-04 23:09:22 -0600

    Update Calculus.java

--- Calculus.java
+++ Calculus.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 /*
  * This class defines several calculus functions
  * 
@@ -8,11 +7,16 @@ import java.util.ArrayList;
  public class Calculus{
 	 //Constructor
 	 public Calculus{}
-	 //integral function lim 1 is limit on the bottom of the integral dx
-	 //determines whether the integral is being taken in terms of x or y
-	 public double integral(double lim1, double lim2, boolean dx, Function f){
+	 //returns the integral of the input function
+	 public String integralFunction(String f){
 		
 	 }
-	 public String
-	 
+	 //returns the derivative function
+	 public String derivativeFunction(String f){
+	 	
+	 }
+	 //returns the integral value
+	 public double integral(lim1, lim2, String f){
+	 	
+	 }
 }
