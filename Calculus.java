a721f615cd359f6e94f119160b1466966e5158dc
seabastion <v8sebas@gmail.com>
2014-03-04 22:51:50 -0600

    Update Calculus.java

--- Calculus.java
+++ Calculus.java
@@ -9,9 +9,10 @@ import java.util.ArrayList;
 	 //Constructor
 	 public Calculus{}
 	 //integral function lim 1 is limit on the bottom of the integral dx
-	 //determines whether the integray is being taken in terms of x or y
-	 public double integral(double lim1, double lim2, boolean dx, String f){
-		 
+	 //determines whether the integral is being taken in terms of x or y
+	 public double integral(double lim1, double lim2, boolean dx, Function f){
+		
 	 }
+	 public String
 	 
 }
